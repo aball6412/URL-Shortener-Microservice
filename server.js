3099c9c9dd9f398ab18f9796e8eae7f4b2eb8f04
aball6412 <aaron.thompkins12@gmail.com>
2016-05-23 14:52:07 -0400

    added static public file for html docs. Added functionality to application

--- server.js
+++ server.js
@@ -9,20 +9,20 @@ var validator = require("validator");
 var mongodb = require("mongodb");
 var MongoClient = mongodb.MongoClient;
 
-var url = "mongodb://localhost:27017/urlShortenerMicroservice"
 
+//Set database connection URL
+var dburl = process.env.MONGOLAB_URI || "mongodb://localhost:27017/urlShortenerMicroservice";
+var collection;
 
 //Set port variable for production or development
 var port = process.env.PORT || 3000;
 
+//Serve the static HTML to the client
+app.use("/", express.static(__dirname + "/public"));
 
 
-
-//Display homepage screen 
-app.get("/", function(request, response) {
-    
-    
-    MongoClient.connect(url, function(err, db) {
+//Initialize the database connection once
+ MongoClient.connect(dburl, function(err, db) {
         if (err) {
             console.log("Unable to connect to database.");
             throw err;
@@ -31,43 +31,72 @@ app.get("/", function(request, response) {
         else {
             console.log("Connection successful.");
             
-            //Do some work
-            
-            db.collection("url").find().toArray(function(err, documents) {
-                
-                console.log(documents);
-            });
+            collection = db.collection("url");
             
       
-            
-           db.close();
         }
 
-    }); //End of database connection
-    
-    
-    response.send("<html><head></head><body><h1>Free Code Camp API Basejump</h1><h2>Url Shortener Microservice</h2></body></html>");
-    
-});
+}); //End of database connection
+
+
+
+//Display homepage screen 
+//app.get("/", function(request, response) {
+//    
+//    response.send();
+//    
+//});
 
 
 //Display original and shortened URL after parameter is passed
 app.get("/*", function(request, response) {
-    
+
     //Get the parameter (which is everything after the first slash no matter the character "/*")
     var url = request.params["0"];
+
     
     //Check to see if URL is valid or not
     if (validator.isURL(url)) {
+        
+        
+            //Check the db to see if it's been used and if not insert
+            var match = "yes";
+
+            
+            //Create random shortener key
+            var key = Math.floor(Math.random() * (10001 - 1) + 1);
+
+
+            collection.find({ short: key }).toArray(function(err, documents) {
+
+
+                if (documents.length >= 1) {
+                    
+                    console.log("That key has already been used"); 
+                    
+                }
+                else {
+                    match = "no";
+                    collection.insert(
+                        {
+                            "original": url,
+                            "short": key
+                        });  
+                    console.log("New URL inserted");
+                }// End else
+
+            }); //End collection query
+            
+        
 
         var display = {
             "Normal URL": url,
-            "Shortened URL": "still working on it"
+            "Shortened URL": key
         }
         
         response.send(display);
         
-    }
+    } //End big if statement
     
     else {
 
@@ -78,8 +107,6 @@ app.get("/*", function(request, response) {
         response.send(display);
     }
     
-    //Google URL Shortener API key
-    //AIzaSyBqSTVVlJSQnZVti7N2hxyCt_GhqR9vznc
     
     
 });
